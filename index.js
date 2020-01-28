4adbcf90fca1ba9773cdf7d8a70f15cc973481cc
Candillan <calvin.baker128@gmail.com>
2020-01-28 07:22:19 -0500

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if (distance <= 400){
+    return "This one is on me!"
+  } else if (distance >= 2000 && distance <= 2500){
+    return "I will gladly take your thirty bucks."
+  } else {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(destination){
   // Write your code here!
+  let result
+  destination === "NYC" ? result = "Ok, sounds good." : result = "No go."
+  return result
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case "generous":
+      return "Thank you so much."
+    case "not as generous":
+      return "Thank you."
+    default:
+      return "Bye."
+  }
 }
